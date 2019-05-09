cd3ae954534904f41b34556065804cf8676ac121
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-05-09 17:23:13 +0530

    Update elmo.py

--- modules/layers/embeddings/elmo.py
+++ modules/layers/embeddings/elmo.py
@@ -1,5 +1,8 @@
 
 from config import device
+import torch
+import torch.nn as nn
+
 
 default_options_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_options.json"
 default_weights_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
