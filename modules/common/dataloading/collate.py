7e425db310466320ddd0ea59e207aa18827d2a69
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-05-10 14:45:48 +0530

    Update collate.py

--- modules/common/dataloading/collate.py
+++ modules/common/dataloading/collate.py
@@ -4,8 +4,8 @@ import torch
 
 def collate_fn_cf(data):
     samples, labels, lengths = zip(*data)
-    labels = torch.cuda.LongTensor(labels)
-    lengths = torch.cuda.LongTensor(lengths)
+    labels = torch.DoubleTensor(labels)
+    lengths = torch.DoubleTensor(lengths)
 
     if isinstance(samples[0], np.ndarray):
         samples = torch.cuda.LongTensor(samples)
