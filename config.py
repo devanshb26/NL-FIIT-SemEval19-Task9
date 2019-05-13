0c15084d734f7b003518361966877728d3cabf10
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-05-14 03:46:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -71,19 +71,19 @@ save_models = [
 ]
 
 ensemble_models = [
-    'best_valid_f1_model',
-    'run_1',
-    'run_2',
-    'run_3',
-    'run_4',
-    'run_5',
-    'run_6',
-    'run_7',
-    'run_8',
-    'run_9',
-    'run_10',
-    'run_11',
-    'run_12',
+#     'best_valid_f1_model',
+#     'run_1',
+#     'run_2',
+#     'run_3',
+#     'run_4',
+#     'run_5',
+#     'run_6',
+#     'run_7',
+#     'run_8',
+#     'run_9',
+#     'run_10',
+#     'run_11',
+#     'run_12',
     'run_13',
     'run_14'
     
