fd9fc72bba1294cd7320fdad1937676266a18569
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-05-13 15:34:46 +0530

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -58,18 +58,18 @@ for model_name in ensemble_models:
     save_predictions(name='submissions/' + model_name, predictions=predicted, original_data=test_data)
     save_predictions_with_probabilities(name='submissions/' + model_name + '_full', predictions=predicted, original_data=test_data, labels=gold_labels, probabilities=model_predictions)
     
-    
+gold_labels_B = test_set_B.labels.astype(int)    
 for model_name in ensemble_models:
     trainer.model = torch.load('checkpoints/' + model_name)
 
-    test_loss, predicted, model_predictions, labels = trainer.evaluate_model(test_loader_B)
+    test_loss_B, predicted_B, model_predictions_B, labels_B = trainer.evaluate_model(test_loader_B)
 
     print('----------------------------------------------------Test results/SubtaskB----------------------------------------------------')
-    print('| Loss: {} | Acc: {}% |'.format(test_loss, accuracy_score(labels, predicted)))
-    print('| Macro Precision: {} | Micro Precision: {} |'.format(precision_score(gold_labels, predicted, average='macro'), precision_score(gold_labels, predicted, average='micro')))
-    print('| Macro Recall: {} | Micro Recall: {} |'.format(recall_score(gold_labels, predicted, average='macro'), recall_score(gold_labels, predicted, average='micro')))
-    print('| Macro F1: {} | Micro F1: {} | Binary F1: {} |'.format(f1_score(gold_labels, predicted, average='macro'), f1_score(gold_labels, predicted, average='micro'), f1_score(labels, predicted)))
+    print('| Loss: {} | Acc: {}% |'.format(test_loss_B, accuracy_score(labels_B, predicted_B)))
+    print('| Macro Precision: {} | Micro Precision: {} |'.format(precision_score(gold_labels_B, predicted_B, average='macro'), precision_score(gold_labels_B, predicted_B, average='micro')))
+    print('| Macro Recall: {} | Micro Recall: {} |'.format(recall_score(gold_labels_B, predicted_B, average='macro'), recall_score(gold_labels_B, predicted_B, average='micro')))
+    print('| Macro F1: {} | Micro F1: {} | Binary F1: {} |'.format(f1_score(gold_labels_B, predicted_B, average='macro'), f1_score(gold_labels_B, predicted_B, average='micro'), f1_score(labels_B, predicted_B)))
     print('--------------------------------------------------------------------------------------------------------------------')
 
-    save_predictions(name='submissions_B/' + model_name, predictions=predicted, original_data=test_data)
-    save_predictions_with_probabilities(name='submissions_B/' + model_name + '_full', predictions=predicted, original_data=test_data, labels=gold_labels, probabilities=model_predictions)
+    save_predictions(name='submissions_B/' + model_name, predictions=predicted_B, original_data=test_data_B)
+    save_predictions_with_probabilities(name='submissions_B/' + model_name + '_full', predictions=predicted_B, original_data=test_data_B, labels=gold_labels_B, probabilities=model_predictions)
