701b7a05b2c383d5552dd2291b8c9cae726d2e58
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-05-13 15:18:33 +0530

    Update dataloading.py

--- modules/common/dataloading/dataloading.py
+++ modules/common/dataloading/dataloading.py
@@ -5,5 +5,6 @@ def load_data(train_file, validation_file, test_file, sep=',', header=0, **kwarg
     train = pd.read_csv(train_file, sep=sep, header=header, quoting=1).sample(frac=1).values
     valid = pd.read_csv(validation_file, sep=sep, header=header, quoting=1).sample(frac=1).values
     test = pd.read_csv(test_file, sep=sep, header=header, quoting=1).values
+    test_B = pd.read_csv(test_file_B, sep=sep, header=header, quoting=1).values
 
-    return train, valid, test
+    return train, valid, test,test_B
