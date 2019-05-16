be6c3feb3e36c3bc0a57627343b6d07fee19ecda
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-05-16 14:44:04 +0530

    Update ensemble_evaluation.py

--- ensemble_evaluation.py
+++ ensemble_evaluation.py
@@ -3,9 +3,10 @@ from torch.utils.data import DataLoader
 import numpy as np
 
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
-
-from config import device, batch_size, encoder_params, data_params, ensemble_models
-
+from modules.layers.embeddings.elmo import ELMo
+from models.rnn_classifier import RNNClassifier
+# from config import device, batch_size, encoder_params, data_params, ensemble_models
+from config import device, ensemble_models,batch_size, model_params, embed_params, encoder_params, transformer_encoder_params, data_params, training_params, paths,save_csv,save_csv_B
 from modules.common.preprocessing import Preprocessing
 from modules.common.utils import class_weigths
 from modules.common.dataloading import load_data, collate_fn_cf
@@ -17,7 +18,7 @@ from utils.submit import save_predictions, save_predictions_with_probabilities
 print('Loading dataset...')
 preprocessing = Preprocessing()
 
-train_data, valid_data, test_data = load_data(**data_params)
+train_data, valid_data, test_data,test_data_B,valid_data_B = load_data(**data_params)
 x_column, y_column = data_params['x_column'], data_params['y_column']
 
 train_set = ClassificationDataset(train_data[:, x_column], train_data[:, y_column], preprocessing=preprocessing.process_text)
@@ -31,7 +32,9 @@ test_loader = DataLoader(test_set, batch_size, collate_fn=collate_fn_cf)
 print('Creating model...')
 weights = class_weigths(train_set.labels).to(device)
 criterion = torch.nn.CrossEntropyLoss(weight=weights)
-
+embeddings = ELMo(**embed_params)
+model = RNNClassifier(embeddings, encoder_params, **model_params).to(device)
+optimizer = torch.optim.Adam(model.parameters())
 trainer = ClassificationTrainer(None, criterion, optimizer, device)
 
 print('Evaluate...')
@@ -46,15 +49,15 @@ for model_name in ensemble_models:
     test_loss, predicted, model_predictions, labels = trainer.evaluate_model(test_loader)
     predictions.append(model_predictions)
     losses.append(test_loss)
-    save_predictions(name='submissions/' + model_name, predictions=predicted, original_data=data.test_data)
-    save_predictions_with_probabilities(name='submissions/' + model_name + '_full', predictions=predicted, original_data=data.test_data, labels=gold_labels, probabilities=model_predictions)
+    save_predictions(name='submissions/' + model_name, predictions=predicted, original_data=test_data)
+    save_predictions_with_probabilities(name='submissions/' + model_name + '_full', predictions=predicted, original_data=test_data, labels=gold_labels, probabilities=model_predictions)
 
 print('Sum ensemble')
 
 sum_predictions = np.stack(predictions).sum(axis=0)
 predicted = np.argmax(sum_predictions, 1)
-save_predictions(name='submissions/ensemble', predictions=predicted, original_data=data.test_data)
-save_predictions_with_probabilities(name='submissions/ensemble_full', predictions=predicted, original_data=data.test_data, labels=gold_labels, probabilities=sum_predictions)
+save_predictions(name='submissions/ensemble', predictions=predicted, original_data=test_data)
+save_predictions_with_probabilities(name='submissions/ensemble_full', predictions=predicted, original_data=test_data, labels=gold_labels, probabilities=sum_predictions)
 
 print('----------------------------------------------------Test results----------------------------------------------------')
 print('| Loss: {} | Acc: {}% |'.format(loss, accuracy_score(labels, predicted)))
@@ -71,8 +74,8 @@ for index, prediction in enumerate(predictions):
         leaderboard[j][prediction[j].argmax()] += 1
 
 predicted = np.argmax(leaderboard, axis=1)
-save_predictions(name='submissions/ensemble_voting', predictions=predicted, original_data=data.test_data)
-save_predictions_with_probabilities(name='submissions/ensemble_voting_full', predictions=predicted, original_data=data.test_data, labels=gold_labels, probabilities=sum_predictions)
+save_predictions(name='submissions/ensemble_voting', predictions=predicted, original_data=test_data)
+save_predictions_with_probabilities(name='submissions/ensemble_voting_full', predictions=predicted, original_data=test_data, labels=gold_labels, probabilities=sum_predictions)
 
 print('----------------------------------------------------Test results----------------------------------------------------')
 print('| Loss: {} | Acc: {}% |'.format(loss, accuracy_score(labels, predicted)))
