34d207e9d4a952bd95b95fdca9746afb3721d291
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-05-18 14:02:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -120,16 +120,16 @@ ensemble_models = [
 #     'run_2',
 #     'run_3',
 #     'run_4',
-#     'run_5',
+    'run_5',
     'run_6',
 #     'run_7',
-    'run_8'
+    'run_8',
 #     'run_9',
 #     'run_10',
 #     'run_11',
 #     'run_12',
-#     'run_13',
-#     'run_15'
+    'run_13',
+    'run_15'
 #     'run_14'
     
 ]
